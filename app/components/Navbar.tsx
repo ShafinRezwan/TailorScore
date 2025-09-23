2d279b9cf41fdd4879070aa3de6ddd41c8674112
ShafinRez <shafinrezwan123@gmail.com>
2025-09-23 13:33:02 -0600

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/">
+        <p className="text-xl font-bold text-gradient">FIX MY RESUME</p>
+      </Link>
+      <Link to="/upload" className="primary-button w-fit font-semibold">
+        Upload Resume
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "~/constants";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "To Job or Not" },
+    { name: "description", content: "Resume Feedback to find you a job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="w-full h-full bg-animated-gradient">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading py-16">
+          <h1 className="text-gradient">
+            Track Your Applications & Resume Ratings
+          </h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
+
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
+      </section>
+    </main>
+  );
 }
